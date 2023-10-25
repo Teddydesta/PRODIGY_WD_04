e4a51c51405a424cc17c3e34f396f85b1207fd8b
Tewodros Desta <teddydesta2021@gmail.com>
2023-10-24 23:59:38 -0700

    update

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import { BrowserRouter,Routes,Route } from "react-router-dom";
 export default function Header() {
   const [isNavOpen, setIsNavOpen] = useState(false);
 
